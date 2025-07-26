53795e814964a53d9985c3b0df8189f9a3ae4c59
Debjyoti Shit <144527817+Debjyoti2004@users.noreply.github.com>
2025-07-26 15:44:39 +0530

    Update runSonarQubeQualityGate.groovy
    increase cache timeout to 20 minutes for long-running tasks in TurboRepo

--- vars/runSonarQubeQualityGate.groovy
+++ vars/runSonarQubeQualityGate.groovy
@@ -5,7 +5,7 @@
 def call() {
     echo "[INFO] Waiting for SonarQube Quality Gate result..."
 
-    timeout(time: 5, unit: 'MINUTES') {
+    timeout(time: 20, unit: 'MINUTES') {
         waitForQualityGate abortPipeline: false
     }
 
